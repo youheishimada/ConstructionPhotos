2e745969c07c928347fcd473d0d665d8f6d586fe
youheishimada <dainoji11@gmail.com>
2025-06-06 18:10:49 +0900

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,8 +12,6 @@ Rails.application.configure do
 
   config.assets.compile = false
 
-  config.active_storage.service = :local
-
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   #config.force_ssl = true
 
