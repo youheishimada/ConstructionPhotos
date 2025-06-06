eabc8be702237e30e4185b942b3f5e4853941453
youheishimada <dainoji11@gmail.com>
2025-06-06 18:33:01 +0900

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,37 +1,28 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-
-  config.enable_reloading = false
-
+  config.cache_classes = true
   config.eager_load = true
-
-  config.consider_all_requests_local = false
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.assets.compile = false
 
+  # 本番はS3を使う場合
+  config.active_storage.service = :amazon
 
-  config.logger = ActiveSupport::Logger.new(STDOUT)
-    .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
-    .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
-
-
+  config.log_level = :info
   config.log_tags = [ :request_id ]
-
-  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
-
 
   config.action_mailer.perform_caching = false
 
   config.i18n.fallbacks = true
-
-
   config.active_support.report_deprecations = false
-
 
   config.active_record.dump_schema_after_migration = false
 
-  config.active_storage.service = :amazon
+  # 必要に応じて下記のSSL/メール/エラー通知などを追記
+  # config.force_ssl = true
+  # config.action_mailer.raise_delivery_errors = false
 end
